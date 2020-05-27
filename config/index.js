8d853720b523e93d0eabc61a5032179418ed17d1
周洋 <zhouyang02@qutoutiao.net>
2020-05-27 15:48:43 +0800

    feat: build

--- config/index.js
+++ config/index.js
@@ -48,8 +48,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
-    assetsSubDirectory: "html/static",
-    assetsPublicPath: "/",
+    assetsSubDirectory: "static",
+    assetsPublicPath: "./",
 
     /**
      * Source Maps
