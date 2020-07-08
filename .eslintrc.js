2fd7e803143a16a7f7b38df4d81ee7fa9752206e
周洋 <zhouyang02@qutoutiao.net>
2020-07-08 11:44:27 +0800

    feat: 引入vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,47 @@ module.exports = {
   plugins: ["vue"],
   // add your custom rules here
   rules: {
+    'indent': ['error', 4], // 缩进宽度4个空格
     // allow async-await
     "generator-star-spacing": "off",
+    'comma-dangle': 'off', // 允许行末逗号
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'no-constant-condition': 'off', // 允许常量作为表达式条件
+    'no-delete-var': 'off', // 允许使用delete
+    'no-extend-native': 'off', // 允许扩展原生对象
+    'no-floating-decimal': 'off', //  允许省去小数点前的0
+    'no-multi-str': 'off', // 允许多行字符串
+    'semi': 'off', // 允许使用分号
+
+    // allow async-await
+    'generator-star-spacing': 'off', // 关闭
+    // 最大长度 120
+    'max-len': [
+        'warn',
+        {
+            'code': 100,
+            'ignoreComments': true,
+            'ignoreStrings': true,
+            'ignoreTemplateLiterals': true,
+            'ignoreUrls': true
+        }
+    ],
+    // 最大行数 500
+    'max-lines': [
+        'warn',
+        {
+            'max': 400,
+            'skipBlankLines': true,
+            'skipComments': true
+        }
+    ],
+    // 要求使用 let 或 const 而不是 var
+    'no-var': [
+        'error'
+    ],
+    "no-eval": 0,//禁止使用eval
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
 };
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -80,6 +80,30 @@ module.exports = {
           name: utils.assetsPath("fonts/[name].[hash:7].[ext]"),
         },
       },
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require('sass'),
+              fiber: require('fibers'),
+              indentedSyntax: true // optional
+            },
+            // Requires sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers'),
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   node: {
